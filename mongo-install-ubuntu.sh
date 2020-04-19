13592feadcc09fe69d13062a5bcb37dce3ed3ef0
JCurtis <41349346+JCurtisss@users.noreply.github.com>
2020-04-19 19:17:20 +1000

    Update mongo-install-ubuntu.sh

--- mongo-install-ubuntu.sh
+++ mongo-install-ubuntu.sh
@@ -20,9 +20,9 @@ sudo bash -c "sudo echo net.ipv4.tcp_keepalive_time = 120 >> /etc/sysctl.conf"
   sudo bash -c " mkdir /data /data/db"
   
   #allow root user to access mongo location
-  sudo chown -R $USER /data/db
-  sudo chown -R $USER /tmp/
-
+  sudo sudo bash -c "chown -R $USER /data/db"
+  sudo sudo bash -c "chown -R $USER /tmp/"
+    
   #sudo bash -c "ufw allow proto tcp from any to any port 27017" #recommend 'from any' to local network range
   #sudo bash -c "ufw enable"  
   
