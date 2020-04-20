838335f705afa40e2f3ad3a84002979f025f9531
JCurtis <41349346+JCurtisss@users.noreply.github.com>
2020-04-21 09:30:00 +1000

    Update mongo-install-ubuntu.sh

--- mongo-install-ubuntu.sh
+++ mongo-install-ubuntu.sh
@@ -17,7 +17,7 @@ sudo bash -c "sudo echo net.ipv4.tcp_keepalive_time = 120 >> /etc/sysctl.conf"
   sudo bash -c "apt autoremove && apt clean"
   
   #Make data location
-  sudo bash -c "mkdir /data /data/db"
+  sudo bash -c " mkdir /data /data/db"
   
   #allow root user to access mongo location
   sudo sudo bash -c "chown -R $USER /data/db"
@@ -27,17 +27,14 @@ sudo bash -c "sudo echo net.ipv4.tcp_keepalive_time = 120 >> /etc/sysctl.conf"
   #sudo bash -c "ufw enable"  
   
   #Remove old sock
-  sudo rm -rf "/tmp/mongodb-27017.sock"
+  sudo rm -rf /tmp/mongodb-27017.sock
   
   #Config
   sudo bash -c "systemctl enable mongod"  #enables Mongo on system startup
   sudo bash -c "service mongod start"
   
   #Create mongo user
-  mongo <<EOF
-  use admin;
-  #db.createUser({'user':'$1','pwd':'$2','roles': ['userAdminAnyDatabase','readWriteAnyDatabase']});
-  EOF
+  #mongo "admin" --eval "db.createUser({'user':'$1','pwd':'$2','roles': ['userAdminAnyDatabase','readWriteAnyDatabase']})"  
   
   #Add Authorization
   sudo bash -c "echo ' ' >> /etc/mongod.conf"
