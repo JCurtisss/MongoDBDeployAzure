1fe828379d1d19d3ee7203148030a514b055932a
JCurtis <41349346+JCurtisss@users.noreply.github.com>
2020-04-21 12:03:38 +1000

    Update mongo-install-ubuntu.sh

--- mongo-install-ubuntu.sh
+++ mongo-install-ubuntu.sh
@@ -42,6 +42,29 @@ sudo bash -c "sudo echo net.ipv4.tcp_keepalive_time = 120 >> /etc/sysctl.conf"
  sudo sed -i -e 's/bindIp: 127.0.0.1/bindIp: 0.0.0.0/g' /etc/mongod.conf
  sudo service mongod restart
  
+ #!/bin/bash
+
+# Initialize a mongo data folder and logfile
+mkdir -p /data/db
+touch /var/log/mongodb.log
+
+# Start mongodb with logging
+# --logpath    Without this mongod will output all log information to the standard output.
+# --logappend  Ensure mongod appends new entries to the end of the logfile. We create it first so that the below tail always finds something
+/usr/bin/mongod  --quiet --logpath /var/log/mongodb.log --logappend &
+
+# Wait until mongo logs that it's ready (or timeout after 60s)
+COUNTER=0
+grep -q 'waiting for connections on port' /var/log/mongodb.log
+while [[ $? -ne 0 && $COUNTER -lt 60 ]] ; do
+    sleep 2
+    let COUNTER+=2
+    echo "Waiting for mongo to initialize... ($COUNTER seconds so far)"
+    grep -q 'waiting for connections on port' /var/log/mongodb.log
+done
+
+# Now we know mongo is ready and can continue with other commands
+ 
  #Create User
  mongo <<EOF
 use admin;
